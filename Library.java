7b5787508e7522352219f6662353858bac2f037f
Vishal Kosaraju <vishalkosaraju@Vishals-MacBook-Pro.local>
2021-02-19 13:22:39 -0500

    Added more changes to the Library class

--- Library.java
+++ Library.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
 	public static Library library;
-	private ArrayList <Library> libraryList;
+	private HashMap <String, Integer> books=new HashMap<String,Integer>();
 	
 	private Library() 
 	{
-		libraryList= Library.getInstance();
+		//library= Library.getInstance();
+		System.out.println("Creating our Library. Time to begin reading");
 	}
 	public static Library getInstance()
 	{
@@ -14,13 +15,45 @@ public class Library {
 			library = new Library();
 		return library;
 	}
+	 public void checkInBook(String bookName,int numToAdd)
+	   {
+	       //Check if book exists
+	       if(books.containsKey(bookName))
+	       {
+	           int count=books.get(bookName);
+	           books.put(bookName, count+numToAdd);
+	       }
+	       else
+	       {
+	           books.put(bookName, numToAdd);
+	       }
+	       System.out.println(bookName+" was added to the library");
+	   }
 	public boolean checkoutBook(String bookName)
 	{
 		
-	}
-	public boolean checkInBook(String bookName)
-	{
+		if(books.containsKey(bookName));
+		{
+			int count= books.get(bookName);
 		
+		if(count>0)
+		{
+			books.put(bookName, count-1);
+			System.out.println(bookName+" was successfully checkout");
+		
+			return true;
+		}
+		else
+			System.out.println("Sorry "+ bookName +" is not in stock");
+		}
+		return false;
 	}
+	 public void displayBooks()
+	   {
+	       System.out.println("Inventory:");
+	       for (HashMap.Entry<String, Integer> entry : books.entrySet()) {
+	           System.out.println("- "+entry.getKey() + ", copies: " + entry.getValue());
+	       }
 
+}
 }
